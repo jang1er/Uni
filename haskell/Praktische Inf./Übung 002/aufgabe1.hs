91dd0dec3949b5f3f448a071849b8323b147205e
jang1er <jokruger03@gmx.de>
2022-11-15 15:07:57 +0100

    First commit

---
+++ haskell/Praktische Inf./Übung 002/aufgabe1.hs
@@ -0,0 +1,13 @@
+lehmann = True
+mueller = True
+schulze = True
+
+-- Aussage 1
+solveL = not schulze
+
+-- Aussage 2
+solveM = not lehmann || not schulze
+
+-- Aussage 3
+solveS = not mueller
+
---
+++ haskell/Praktische Inf./Übung 002/aufgabe2.hs
@@ -0,0 +1,11 @@
+-- teilaufgabe a
+fak_rec n = if n == 0 then 1
+            else n * fak_rec (n-1)
+-- Typsignatur: Num t => t -> t
+
+-- teilaufgabe b
+fak_end n m = if n == 0 then m 
+                else  fak_end (n-1) (n * m)
+-- Typsignatur: Num t => t -> t -> t
+
+-- teilaufgabe c 
