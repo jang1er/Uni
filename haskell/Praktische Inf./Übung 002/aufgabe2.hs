76aaf28bb202ebb3f0181b0b79d8849163292ffe
jang1er <jokruger03@gmx.de>
2022-11-20 14:08:03 +0100

    verbesserung aufgabe2.hs

--- haskell/Praktische Inf./Übung 002/aufgabe2.hs
+++ haskell/Praktische Inf./Übung 002/aufgabe2.hs
@@ -1,9 +1,12 @@
+
 -- teilaufgabe a
+fak :: Integer -> Integer
 fak 0 = 1
 fak n = n * fak (n - 1)
 -- Typsignatur: fak :: Int -> Int
 
 -- teilaufgabe b
+fakEnd :: Integer -> Integer -> Integer
 fakEnd 0 m = m
 fakEnd n m = fakEnd (n-1) (n * m)
 -- Typsignatur: fakEnd :: Int -> Int -> Int
@@ -23,10 +26,12 @@ isOdd n = mod n 2 == 1
 -- Typsignatur: isOdd :: Int -> Bool
 
 -- teilaufgabe e
+isEvenR :: Integer -> Bool
 isEvenR 0 = True
 isEvenR n = isOddR (n-1)
 -- Typsignatur : isEvenR :: Int -> Bool
 
+isOddR :: Integer -> Bool
 isOddR 0 = False 
 isOddR n = isEvenR (n-1)
 -- Typsignatur : isOddR :: Int -> Bool
