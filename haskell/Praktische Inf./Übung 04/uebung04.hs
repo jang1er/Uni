3a9c8e8e893df5fc9e23bae30ed7a4608e950dfc
jang1er <87527531+jang1er@users.noreply.github.com>
2022-12-20 12:00:27 +0000

    Verbesserung Übung04

--- haskell/Praktische Inf./Übung 04/uebung04.hs
+++ haskell/Praktische Inf./Übung 04/uebung04.hs
@@ -39,35 +39,34 @@ instance Eq Card where
 -- f)
 
 trick :: Suit -> Card -> Card -> Card 
-trick t (Of r1 s1) (Of r2 s2)  | s1 == t && s2 /= t = (Of r1 s1)
-                                    | s1 /= t && s2 == t = (Of r2 s2)
-                                    | otherwise = if (r1 <= r2)
-                                                    then (Of r2 s2)
-                                                    else (Of r1 s1)
+trick t (Of r1 s1) (Of r2 s2)   | s1 == t && s2 /= t = (Of r1 s1)
+                                | s1 /= t && s2 == t = (Of r2 s2)
+                                | otherwise = if (r1 <= r2)
+                                            then (Of r2 s2)
+                                            else (Of r1 s1)
 
 -- g)
 -- Vielleicht geht das besser, aber ist mir egal
-turn :: Card -> Card -> (Int , Int) -> (Int, Int)
-turn a b (sc1, sc2) | sc1 > sc2 = if ((trick (getSuit b ) a b) == a)
+turn :: (Card, Card) -> (Int , Int) -> (Int, Int)
+turn (a, b) (sc1, sc2) | sc1 > sc2 = if ((trick (getSuit b ) a b) == a)
                                     then (sc1 + 1, sc2 )
                                     else (sc1 ,sc2 + 1 )
-                    | sc1 < sc2 = if ((trick (getSuit a ) a b ) == a)
+                    | otherwise = if ((trick (getSuit a ) a b ) == a)
                                     then (sc1 + 1, sc2)
                                     else (sc1 , sc2 + 1)
-                    | otherwise = if ((trick (getSuit a ) a b) == a)
-                                    then (sc1 + 1, sc2 )
-                                    else (sc1 ,sc2 + 1 )
 -- Hilffunktionen:
 getSuit :: Card -> Suit
 getSuit (Of _ suit ) = suit
 
 -- f)
 game :: [(Card, Card)] -> (Int, Int)
-game a = gameStart a (0,0)
+game [] = (0,0)
+game a = turn (last a) (game (init a))
 
 gameStart :: [(Card, Card)] -> (Int, Int) -> (Int, Int)
-gameStart ((a , b) : cards) (sc1 , sc2) | cards == [] = turn a b (sc1, sc2)
-                                        | otherwise = gameStart cards (turn a b (sc1,sc2))
+gameStart ((a , b) : cards) (sc1 , sc2) | cards == [] = turn (a, b) (sc1, sc2)
+                                        | otherwise = gameStart cards (turn (a, b) (sc1,sc2))
+
 
 {- ###########################################
 
