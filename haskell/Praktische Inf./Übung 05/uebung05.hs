9b514bbafeeaf071528b8331f371294fee7f8964
jang1er <87527531+jang1er@users.noreply.github.com>
2023-01-12 10:24:45 +0000

    Übung05

--- haskell/Praktische Inf./Übung 05/uebung05.hs
+++ haskell/Praktische Inf./Übung 05/uebung05.hs
@@ -9,3 +9,25 @@ take' n (x:xs) = (x: take' (n-1) xs)
 drop' :: Int -> [a] -> [a]
 drop' 0 a = a
 drop' n (x:xs) = drop (n-1) xs
+
+-- c)
+split' :: Integer -> [a] -> ([a],[a])
+split' _ [] = ([], [])
+split' 1 (x:xs) = ([x],xs)
+split' n (x:xs) = (x:xs', xs'')
+    where (xs' , xs'') = split' (n-1) xs
+
+-- d)
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x] 
+
+-- e)
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' f (x:xs) | (f x) = ( x : takeWhile' f xs)
+                    | otherwise = []
+
+-- f)
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' f (x:xs) | (f x) = dropWhile' f xs
+                    | otherwise = (x:xs)
