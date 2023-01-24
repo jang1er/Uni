820be37271f50dd618070f56f77d2794cb8cd3f6
jang1er <87527531+jang1er@users.noreply.github.com>
2023-01-24 13:25:32 +0100

    Create FolderTree.hs

---
+++ haskell/Praktische Inf./Übung 06/FolderTree.hs
@@ -0,0 +1,114 @@
+import Data.List (find, deleteBy)
+
+type Name = String
+type Path = [Name]
+
+data FolderTree content
+  = Content Name content
+  | Folder Name [FolderTree content]
+  deriving (Show, Eq, Ord)
+
+getName :: FolderTree c -> Name
+getName (Content n _) = n
+getName (Folder n _)  = n
+
+data FileType = Binary | Text
+  deriving (Show, Eq)
+
+type Size = Int
+
+data File = File FileType Size
+  deriving (Show, Eq)
+
+
+example = Folder "/"
+  [ Folder "bin"
+    [ Content "cp" (File Binary 140) 
+    , Content "mv" (File Binary 159)
+    , Content "edit" (File Binary 2293784)
+    ]
+  , Folder "docs"
+    [ Content "letter" (File Text 105)
+    , Content "notes" (File Text 5056)
+    , Folder "work" 
+      [ Content "todos" (File Text 198571)
+      , Content "notes" (File Text 1238)
+      , Content "tetris" (File Binary 8230987)
+      ]
+    ]
+  ]
+
+
+foldFolderTree :: (Name -> content -> b) -> (Name -> [b] -> b)
+               -> FolderTree content -> b
+foldFolderTree fContent fFolder = fold
+  where
+    fold (Content n c) = fContent n c
+    fold (Folder n fs) = fFolder n (map fold fs)
+
+
+sizeOfType :: FileType -> FolderTree File -> Size
+sizeOfType t = undefined
+
+
+filesOfType :: FileType -> FolderTree File -> [Path]
+filesOfType t = undefined
+
+
+
+data FolderTreeZip content
+  = FolderZip String [FolderTree content]
+  deriving (Show, Eq)
+
+type Zipper content = (FolderTree content, [FolderTreeZip content])
+
+
+zipper :: FolderTree content -> Zipper content
+zipper f = (f, [])
+
+
+descend :: String -> Zipper content -> Maybe (Zipper content)
+descend _ (Content _ _, _)   = Nothing
+descend n (Folder n' fs, zs) = case find (\x -> getName x == n) fs of
+    Nothing -> Nothing
+    Just f  -> Just (f, FolderZip n' (deleteBy (\a b -> getName a == getName b) f fs) : zs)
+
+
+descendTo :: Path -> Zipper c -> Maybe (Zipper c)
+descendTo [] z     = Just z
+descendTo (n:ns) z = maybe Nothing (descendTo ns) (descend n z)
+
+
+ascend :: Zipper content -> Zipper content
+ascend (t, FolderZip n ts : zs) = (Folder n (t:ts), zs)
+
+
+unwind :: Zipper content -> FolderTree content
+unwind (t, []) = t
+unwind z       = unwind (ascend z)
+
+
+modify :: (c -> c) -> Zipper c -> Maybe (Zipper c)
+modify f (Content n x, zs) = Just (Content n (f x), zs)
+modify f _                 = Nothing
+
+
+get :: (c -> a) -> Zipper c -> Maybe a
+get f (Content _ x, _) = Just (f x)
+get _ _                = Nothing
+
+
+getAt :: Path -> (c -> a) -> FolderTree c -> Maybe a
+getAt p f = undefined
+
+
+modifyAt :: Path -> (c -> c) -> FolderTree c -> Maybe (FolderTree c)
+modifyAt p f = undefined
+
+
+isFileOfType :: Path -> FileType -> FolderTree File -> Maybe Bool
+isFileOfType p t = undefined
+
+
+setTypeOfFile :: Path -> FileType -> FolderTree File -> Maybe (FolderTree File)
+setTypeOfFile p t = undefined
