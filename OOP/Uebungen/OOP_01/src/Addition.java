2143039187f9606700a5d8555f54b72c3253d5f7
jang1er <jonas.kruger@uni-ulm.de>
2023-04-25 17:44:18 +0200

    Übung 1 , Aufgabe 1

---
+++ OOP/Uebungen/OOP_01/src/Addition.java
@@ -0,0 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Addition {
+
+    //Blackbox
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Deklarierung: Variable wird erstellt, Datentyp und Name werden festgelegt
+        // Initialisierung: Variable bekommt den ersten Wert des entsprechenden Datentyps zugewiesen
+        int myInteger = readIntFromConsole();
+
+        // zweite Variable
+        int myOtherInteger = readIntFromConsole();
+
+        int mySum = myInteger + myOtherInteger;
+
+        System.out.println("Eingabe1: " + myInteger);
+        System.out.println("Eingabe2: " + myOtherInteger);
+        System.out.println("Summe: " + mySum);
+    }
+
+    /**
+     * Blackbox-Methode zum Einlesen aus der Konsole. Bei einer fehlerhaften Eingabe wird zu einer
+     * erneuten Eingabe aufgefordet,
+     *
+     * @return Bei korrekter Eingabe, die Eingabe als int
+     */
+    public static int readIntFromConsole() {
+
+        System.out.print("Please insert an integer: ");
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException ime) {
+            System.out.println("[Error] Input is not a whole number!");
+            scanner.next();
+        }
+
+        return readIntFromConsole();
+    }
+}
