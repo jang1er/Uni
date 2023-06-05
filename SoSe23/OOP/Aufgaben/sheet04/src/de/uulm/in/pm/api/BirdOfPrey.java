f494a589271471828d31a3a0abe24268a7b431c6
jonas <jonas.kruger@uni-ulm.de>
2023-06-05 17:04:01 +0200

    OOP sheet04 finished + even better overall filestructure for OOP

---
+++ SoSe23/OOP/Aufgaben/sheet04/src/de/uulm/in/pm/api/BirdOfPrey.java
@@ -0,0 +1,7 @@
+package de.uulm.in.pm.api;
+
+/** Interface for birds of prey. */
+public interface BirdOfPrey {
+
+  public void hunt();
+}
---
+++ SoSe23/OOP/Aufgaben/sheet04/src/de/uulm/in/pm/api/Flyable.java
@@ -0,0 +1,11 @@
+package de.uulm.in.pm.api;
+
+/** Things that can fly. */
+public interface Flyable {
+
+  public boolean isFlying();
+
+  public void takeOff();
+
+  public void land();
+}
